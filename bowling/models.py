feb55dafd897c0a1ac91a8e37e6ce8f5b3c2c446
Quentin McRee <qmcree@gmail.com>
2016-01-23 03:45:09 -0600

    - Defined models - Added app to project settings - Added initial migration

--- bowling/models.py
+++ bowling/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    created_at = models.DateTimeField()
+
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=40)
+
+
+class Delivery(models.Model):
+    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    frame = models.SmallIntegerField() # max_length=10
+    pins_hit = models.SmallIntegerField(default=0) # max_length=10
+    created_at = models.DateTimeField()
+
+
+class GameScore(models.Model):
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
+    score = models.SmallIntegerField() # max_length=300
